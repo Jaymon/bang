d959f70e96c733dafcb4c6fc11853bc03553c405
Jay Marcyes <jay@marcyes.com>
2023-01-02 17:17:30 -0700

    Closes #59 by removing the slashes in the opengraph plugin

--- bang/plugins/opengraph.py
+++ bang/plugins/opengraph.py
@@ -17,11 +17,11 @@ def template_output_ga(event, config):
 
     s = [
         '<meta name="description" content="{}">'.format(instance.description),
-        '<meta property="og:url" content="{}" />'.format(instance.url),
-        '<meta property="og:type" content="article" />',
-        '<meta property="og:title" content="{}" />'.format(instance.title),
-        '<meta property="og:description" content="{}" />'.format(instance.description),
-        '<meta property="og:image" content="{}" />'.format(instance.image),
+        '<meta property="og:url" content="{}">'.format(instance.url),
+        '<meta property="og:type" content="article">',
+        '<meta property="og:title" content="{}">'.format(instance.title),
+        '<meta property="og:description" content="{}">'.format(instance.description),
+        '<meta property="og:image" content="{}">'.format(instance.image),
     ]
 
     event.html = event.html.inject_into_head("\n".join(s))
