e12fa84990966d35a4eb4b55ec87faf84f256141
Jay Marcyes <jay@marcyes.com>
2023-08-22 14:50:30 -0600

    Removes comment out breadcrumbs code

--- bang/plugins/breadcrumbs/__init__.py
+++ bang/plugins/breadcrumbs/__init__.py
@@ -35,46 +35,6 @@ def configure_context_breadcrumbs(event, config):
         theme.template_dirs.append(dd.child_dir("template"))
 
 
-# @event('context.breadcrumbs')
-# def configure_context_breadcrumbs(event, config):
-#     # we do this here so the project has a chance to set a theme
-#     theme_name = config.theme_name
-#     theme = config.theme
-#     if not theme.has_template("breadcrumbs"):
-#         dd = DataDirpath(__name__)
-#         config.add_themes(dd.themes_dir())
-#         theme_name = "breadcrumbs"
-# 
-#     config.setdefault("breadcrumbs_theme_name", theme_name)
-
-
-# @event('output.finish')
-# def output_breadcrumbs(event, config):
-#     with config.context("breadcrumbs") as config:
-# 
-#         d = defaultdict(list)
-#         for p in config.breadcrumbs_iter:
-#             for bc in p.url.paths:
-#                 d[bc].append(p)
-# 
-#         theme = config.themes[config.breadcrumbs_theme_name]
-#         basename = config.page_output_basename
-# 
-#         for breadcrumb, instances in d.items():
-#             path = config.output_dir.child_dir(breadcrumb)
-# 
-#             # we only want to add breadcrumb files to directories that don't
-#             # have any index files already
-#             if not path.has_file(basename):
-#                 logger.debug("Generating breadcrumb for {}".format(breadcrumb))
-#                 theme.output_template(
-#                     "breadcrumbs",
-#                     path.child_file(basename),
-#                     breadcrumb=breadcrumb,
-#                     path=path,
-#                     instances=instances,
-#                 )
-
 @event('output.finish')
 def output_breadcrumbs(event, config):
     with config.context("breadcrumbs") as config:
